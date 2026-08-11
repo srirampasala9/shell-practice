6a03da805ec23bd48717aabff3375b96bf4819bf
Sriram <sri.pasala9@gmail.com>
2026-08-11 10:52:03 +0530

    passing the variables while running the script

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#shebang
+
+#This is for practicing to pass the parameters while running the code $1 is one parameter and $2 is another paarmeter like this we can pass many parameters as we want
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: How are you $PERSON2"
+echo "$PERSON2:: I am good $PERSON1"
