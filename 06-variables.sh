c37e5e2209f3e543d720b4c73c1c3b37d6c1255f
Sriram <sri.pasala9@gmail.com>
2026-08-12 11:09:20 +0530

    variables

--- 06-variables.sh
+++ 06-variables.sh
@@ -8,6 +8,6 @@ sleep 10 # It will wait 10 sec after that execute the next line
 
 END_TIME=$(date +%s) #Give Small "s" it will print the time in sec
 
-TOTAL_TIME= $(($START_TIME-$END_TIME)) # Give double brackets other wise "$() the text inside the brackets will be treated as command so we have to give double brackets"
+TOTAL_TIME=$(($START_TIME-$END_TIME)) # Give double brackets other wise "$() the text inside the brackets will be treated as command so we have to give double brackets"
 
 echo "Script Executed in : $TOTAL_TIME Seconds"
