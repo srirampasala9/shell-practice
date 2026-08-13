0f82abc53da9703f4d1a13cb3ae14b2b3c63aa3c
Sriram <sri.pasala9@gmail.com>
2026-08-13 11:35:11 +0530

    variables

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+ 
+#Everything in shell considered as string in shell
+
 NUMBER1=100
 NUMBER2=200
 
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo "SUM Of Two Numbers is: ${SUM}"
+
+#Arrays
+#Size of the Array=4 , Index of the Array =3
+LEADERS=("Modi" "Putin" "Trudo" "Trump")
+
+echo "All Leaders: $($LEADERS[@])"
+echo "First Leader: $($LEADERS[0])"
+echo "Second Leader: $($LEADERS[1])"
