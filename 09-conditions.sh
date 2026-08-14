387f6136ea0a89ad4b25643db2044ebe26384c2b
Sriram <sri.pasala9@gmail.com>
2026-08-14 12:03:23 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -3,7 +3,7 @@
 #Condtions
 
 NUMBER=$1
-if [ $NUMBER -lt 10 ] then; #B/W the brackets there should be space
+if [ $NUMBER -lt 10 ]; then #B/W the brackets there should be space
     echo "The Given $NUMBER is less than 10"
 else
     echo "The Given $NUMBER is greater than or equal to 10"
