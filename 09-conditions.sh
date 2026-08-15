50b3027f23821a85df6b6845ff848b16bacbacf8
Sriram <sri.pasala9@gmail.com>
2026-08-15 22:55:24 +0530

    practice

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -9,3 +9,9 @@ else
     echo "The Given number $NUMBER is greater than or equal to 10"
 fi 
 
+#If the Given Number matches
+if [ $NUMBER-eq 10]; then
+    echo "The Given Number $NUMBER Matches the Number 10"
+else
+    echo "The Given Number Not Matched"
+fi
