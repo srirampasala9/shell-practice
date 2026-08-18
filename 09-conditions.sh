d83bf1a7d9acdea48e7eb4ede4335a27a46a6db1
Sriram <sri.pasala9@gmail.com>
2026-08-18 11:57:35 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,8 +5,10 @@
 NUMBER=$1
 if [ $NUMBER -lt 10 ]; then #B/W the brackets there should be space
     echo "The Given number $NUMBER is less than 10"
-else
-    echo "The Given number $NUMBER is greater than or equal to 10"
+    
 elif[ $NUMBER -eq 10 ]; then
     echo "The Given number $NUMBER is equal to 10"
+else
+    echo "The Given number $NUMBER is greater than or equal to 10"
+
 fi 
