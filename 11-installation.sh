ee6081b311213b2f389ce17135ade6a958e1ab1a
Sriram <sri.pasala9@gmail.com>
2026-08-19 11:45:45 +0530

    Installtions

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+#First check the user have the root  access 
+#How to check the user have the root  access or not
+  #If the user id is 0 then we can confirm that the user has root access
+
+
 USERID=$(id -u)
-echo $USERID
+
+if [ $USERID -ne 0]; then
+    echo "ERROR:: User does not have the root access"
+else
+    echo "User have the root access"
