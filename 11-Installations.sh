2186b707254b5b18c4b23a61f97b0b92fa891a07
Sriram <sri.pasala9@gmail.com>
2026-09-02 11:48:40 +0530

    Installations

---
+++ 11-Installations.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ USERID -eq 0 ]; then
+    dnf install mysql -y
+else 
+    echo "ERROR::Unable to install the user does not have the root have access, to install mysql run this script with the root access"
+    
