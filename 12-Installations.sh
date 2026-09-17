d29e73c60d713f5ed098c225de975fd452e40016
Sriram <sri.pasala9@gmail.com>
2026-09-17 11:43:05 +0530

    Install

---
+++ 12-Installations.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#check whether the user has root access or not
+
+USERID=$(id -u) # If the user has root access then the value will be 0
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: User does not have the root access please run the script with root privelege"
+    exit 1 #If we get the error then it will return the exit status code as 1
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "ERROR::$2 Installation failure"
+        exit 1
+    else
+        echo "Installing $2 success"
+    fi
+}
+#Instatlling Mysql
+
+dnf install mysql -y
+VALIDATE $1 "MYSQL"
+
+#Installing Nginx
+
+dnf install nginx -y
+VALIDATE $1 "NGINX"
+
+
+#Installing Python
+dnf install python3 -y
+VALIDATE $1 "Python"
