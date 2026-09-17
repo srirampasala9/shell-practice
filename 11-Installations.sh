24f7d5f6662742fb07ed22c6a85ceaa7394ad3ec
Sriram <sri.pasala9@gmail.com>
2026-09-17 10:49:25 +0530

    Practice-installation

--- 11-Installations.sh
+++ 11-Installations.sh
@@ -6,6 +6,7 @@ USERID=$(id -u) # If the user has root access then the value will be 0
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: User does not have the root access please run the script with root privelege"
+    exit 1
 fi
 
 dnf install mysql -y
